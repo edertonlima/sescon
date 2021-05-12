b6a0fdfb848e99733666c483581326a6aad31488
Ederton Lima <edertton@gmail.com>
2021-05-12 15:52:30 -0300

    reset wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define( 'DB_NAME', 'edertton_sescon' );
+define( 'DB_NAME', '' );
 
 /** Usuário do banco de dados MySQL */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', '' );
 
 /** Senha do banco de dados MySQL */
 define( 'DB_PASSWORD', '' );
